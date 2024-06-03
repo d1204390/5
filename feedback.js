97b9e25fd663e24c9998258fd3069ccb028e186e
d1204390 <d1204390@o365.fcu.edu.tw>
2024-06-03 22:08:29 +0800

    Initial commit

---
+++ feedback.js
@@ -0,0 +1,6 @@
+
+
+function displayMessage() {
+    alert("回報成功！謝謝您的回饋。");
+}
+
---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+function goFanPage() {
+    window.location.href = "https://www.facebook.com/ShopeeTW"; // 蝦皮粉絲專頁的 URL
+}
+
+function goHelpCenter() {
+    window.location.href = "https://help.shopee.tw/tw/s/"; // 蝦皮幫助中心的 URL
+}
+
+function goHome() {
+    window.location.href = "https://shopee.tw/"; // 官網首頁的 URL
+}
